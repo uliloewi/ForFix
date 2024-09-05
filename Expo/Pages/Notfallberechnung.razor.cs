abd78fa5ff2bcb7c8ea836301690b660ed1e059e
uliloewi <zequanyu@yahoo.de>
2024-09-05 14:55:01 +0200

    show new chart seecessfully

--- Expo/Pages/Notfallberechnung.razor.cs
+++ Expo/Pages/Notfallberechnung.razor.cs
@@ -6,19 +6,25 @@ namespace Expo.Pages
 {
     public partial class Notfallberechnung : ComponentBase
     {
-        private string currentChartPath = "./chart/Mode-5000.bmp";
+        private string currentChartPath;
         public async void CreateChartAsync()
         {
             var n = new Bitmap(314, 314);
             using (var g = Graphics.FromImage(n))
-            {                   
+            {
                 g.DrawString("hallo test", new System.Drawing.Font("Tahoma", 8), new SolidBrush(System.Drawing.Color.Red), 200, 200);
             }
             int length = AppDomain.CurrentDomain.BaseDirectory.LastIndexOf(AppDomain.CurrentDomain.FriendlyName) + AppDomain.CurrentDomain.FriendlyName.Length + 1;
-            string sub = AppDomain.CurrentDomain.BaseDirectory.Substring(0, length);
-            var fileName = sub + "wwwroot\\chart\\Mode-5000.bmp";
-            n.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg);              
+            //string sub = AppDomain.CurrentDomain.BaseDirectory.Substring(0, length);
+            string sub = Path.Combine(AppContext.BaseDirectory, "wwwroot", "chart");
+            if (!Directory.Exists(sub))
+            {
+                Directory.CreateDirectory(sub);
+            }
+            var fileName = Path.Combine(sub, "Mode-5000.bmp");
+            n.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
             StateHasChanged();
+            currentChartPath = "./chart/Mode-5000.bmp";
         }
     }
 }
